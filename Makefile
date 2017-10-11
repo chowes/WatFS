ea8a6bb16313ac3269e2b217db9c0a4cfda578ca
chowes <chowes@uwaterloo.ca>
2017-10-11 11:33:42 -0400

    Implemented Null, fixed some Makefile problems

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CXX = g++
 CPPFLAGS += `pkg-config --cflags protobuf grpc fuse3`
-CXXFLAGS += -std=c++11
+CXXFLAGS += -std=c++11 -I.
 LDFLAGS += -L/usr/local/lib `pkg-config --libs protobuf grpc++ fuse3`
 
 PROTOC = protoc
@@ -9,24 +9,25 @@ GRPC_CPP_PLUGIN_PATH ?= `which $(GRPC_CPP_PLUGIN)`
 
 PROTOS_PATH = proto
 SRC_PATH = src
-INCLUDE_PATH = include
 
 
 vpath %.proto $(PROTOS_PATH)
 vpath %.cc $(SRC_PATH)
-vpath %.h $(INCLUDE_PATH)
 
 
-all: watfs_client watfs_server watfs_grpc_server
+all: watfs_client watfs_server watfs_grpc_server watfs_grpc_client
 
 watfs_client: watfs_client.o
-	$(CXX) $^ $(LDFLAGS) -o $@
+	$(CXX) $^ -I. $(LDFLAGS) -o $@
 
 watfs_server: watfs_server.o
-	$(CXX) $^ $(LDFLAGS) -o $@
+	$(CXX) $^ -I. $(LDFLAGS) -o $@
 
-watfs_grpc_server: watfs_grpc_server.o
-	$(CXX) $^ $(LDFLAGS) -o $@
+watfs_grpc_server: watfs.pb.o watfs.grpc.pb.o watfs_grpc_server.o
+	$(CXX) $^ -I. $(LDFLAGS) -o $@
+
+watfs_grpc_client: watfs.pb.o watfs.grpc.pb.o watfs_grpc_client.o
+	$(CXX) $^ -I. $(LDFLAGS) -o $@
 
 %.grpc.pb.cc: %.proto
 	$(PROTOC) -I $(PROTOS_PATH) --grpc_out=. --plugin=protoc-gen-grpc=$(GRPC_CPP_PLUGIN_PATH) $<
@@ -35,4 +36,4 @@ watfs_grpc_server: watfs_grpc_server.o
 	$(PROTOC) -I $(PROTOS_PATH) --cpp_out=. $<
 
 clean:
-	rm -f *.o *.pb.cc *.pb.h watfs_client watfs_server watfs_grpc_server
+	rm -f *.o *.pb.cc *.pb.h watfs_client watfs_server watfs_grpc_server watfs_grpc_client
--- src/watfs_grpc_client.cc
+++ src/watfs_grpc_client.cc
@@ -2,34 +2,64 @@
 #include <memory>
 #include <string>
 
-#include <boost/program_options.hpp>
 #include <grpc++/grpc++.h>
+#include <grpc++/channel.h>
+#include <grpc++/client_context.h>
+#include <grpc++/create_channel.h>
+#include <grpc++/security/credentials.h>
+#include "watfs.grpc.pb.h"
+
+using watfs::WatFS;
+using watfs::WatFSStatus;
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientReaderWriter;
+using grpc::ClientWriter;
+using grpc::Status;
+using watfs::WatFS;
+
+class WatFSClient {
+public:
+    WatFSClient(std::shared_ptr<Channel> channel)
+            : stub_(WatFS::NewStub(channel)) {}
 
 
+    /* 
+     * call WatFSNull to ping the server
+     *
+     * return: response from server on success
+     *         -1 on failure
+     */
+    bool WatFSNull() {
+        ClientContext context;
+        WatFSStatus client_status;
+        WatFSStatus server_status;
+
+        client_status.set_status(0);
+
+        Status status = stub_->WatFSNull(&context, client_status, 
+                                         &server_status);
+
+        if (!status.ok() || server_status.status() != client_status.status()) {
+            std::cerr << "WatFSNull: rpc failed." << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+private:
+    std::unique_ptr<WatFS::Stub> stub_;
+};
 
 int main(int argc, const char *argv[])
 {
-    try {
-        options_description desc{"Options"};
-        desc.add_options()
-        ("help,h", "Help screen")
-        ("pi", value<float>()->default_value(3.14f), "Pi")
-        ("age", value<int>()->notifier(on_age), "Age");
+    WatFSClient client(grpc::CreateChannel("localhost:8080", 
+                                           grpc::InsecureChannelCredentials()));
 
-        variables_map vm;
-        store(parse_command_line(argc, argv, desc), vm);
-        notify(vm);
 
-        if (vm.count("help"))
-            std::cout << desc << '\n';
-        else if (vm.count("age"))
-            std::cout << "Age: " << vm["age"].as<int>() << '\n';
-        else if (vm.count("pi"))
-            std::cout << "Pi: " << vm["pi"].as<float>() << '\n';
-    } catch (const error &ex) {
-        std::cerr << ex.what() << '\n';
-    }
-    
+    std::cout << client.WatFSNull() << std::endl;
 
     return 0;
 }
